d3f6af12519c1eaaea38a41902a49b9ffd6a9ae9
Ramyadev <ramya.devendhiran@greeno.in>
2014-10-17 18:11:08 +0530

    Alert box

--- CrashReport/src/com/greeno/crashreport/CrashReportActivity.java
+++ CrashReport/src/com/greeno/crashreport/CrashReportActivity.java
@@ -12,14 +12,22 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.TextView;
 
 import com.greeno.Data.CrashReportData;
 import com.greeno.Util.ApplicationInformation;
@@ -27,6 +35,7 @@ import com.greeno.Util.XmlParser;
 import com.greeno.model.CrashReportDB_Factory;
 
 public class CrashReportActivity extends Activity {
+	LinearLayout layout;
 	Button okButtonCrash, cancelButtonCrash ;
 	ProgressDialog progressBar;
 	CrashReportData crashReportData = new CrashReportData();
@@ -40,18 +49,12 @@ public class CrashReportActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setFinishOnTouchOutside (false);
-        setContentView(R.layout.floatingactivity);
-        okButtonCrash = (Button)findViewById(R.id.okButtonCrash);
-        cancelButtonCrash = (Button)findViewById(R.id.cancelButtonCrash);
-       
-        
+
         crashReportDB_Factory = new CrashReportDB_Factory(getBaseContext());
         progressBar = new ProgressDialog(this);
 		progressBar.setCancelable(true);
 		progressBar.setProgressStyle(ProgressDialog.THEME_HOLO_DARK);
-        
-//        Bundle bundle = getIntent().getExtras();
-       
+		System.out.println("======== Entering CrashReportActivity ==");
         try {
 			crashReportData = crashReportDB_Factory.getReportByCurrentDate(applicationInformation.getApplicationName(getBaseContext()), crashReportDB_Factory.getLastDataCurrentDate(applicationInformation.getApplicationName(getBaseContext())));
 		} catch (NameNotFoundException e) {
@@ -59,8 +62,101 @@ public class CrashReportActivity extends Activity {
 			e.printStackTrace();
 		}
         
-        /** If user clicks OK, Send details to Server */
-       ((Button)findViewById(R.id.okButtonCrash)).setOnClickListener(new OnClickListener() {
+        openAlertDialog();
+        
+ /*       
+        layout = new LinearLayout(this);
+        LinearLayout layoutSub = new LinearLayout(this);
+        
+        TextView textViewHeading = new TextView(this);
+        TextView textViewContent = new TextView(this);
+        okButtonCrash = new Button(this);
+        cancelButtonCrash = new Button(this);
+        
+        Display display = getWindowManager().getDefaultDisplay();
+		int screenWidth = display.getWidth();
+		int screenHeight = display.getHeight(); 
+		System.out.println("screenWidth="+screenWidth+";screenHeight="+screenHeight);		
+		int alertBoxWidth = (screenWidth * 1)/3;
+		int alertBoxHeight = (screenHeight * 1)/3;
+        
+        
+        layout.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        layout.setOrientation(LinearLayout.VERTICAL);
+//        LinearLayout.LayoutParams paramsHead = new LinearLayout.LayoutParams(
+                LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        layout.setLayoutParams(paramsHead);
+//	
+        textViewHeading.setText(" ALERT ");
+        textViewHeading.setPadding(5, 20, 5, 20);
+        textViewHeading.setTextColor(Color.parseColor("#FFFFFF"));
+        textViewHeading.setBackgroundColor(Color.parseColor("#007fff"));
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LayoutParams.MATCH_PARENT,(alertBoxHeight*1)/4);//50);
+//            params.weight = 1.0f;
+            params.gravity=Gravity.CENTER;
+
+        textViewHeading.setLayoutParams(params);
+        textViewHeading.setTextSize(16);
+        
+        
+
+        
+        
+        LinearLayout.LayoutParams paramsSub = new LinearLayout.LayoutParams(
+                LayoutParams.WRAP_CONTENT,  (alertBoxHeight*2)/4);//100);
+//            params.weight = 1.0f;
+        paramsSub.gravity=Gravity.CENTER;
+        textViewContent.setLayoutParams(paramsSub);
+        textViewContent.setText("Looks like something went wrong, Sorry for the inconvenience. We will analyze the issue.");
+        textViewContent.setPadding(5, 10, 5, 20);
+        textViewContent.setTextColor(Color.parseColor("#000000"));
+        textViewContent.setTextSize(14);
+        textViewContent.setGravity(Gravity.CENTER);
+        
+
+        LinearLayout.LayoutParams paramsLayoutSub = new LinearLayout.LayoutParams(
+        		alertBoxWidth, (alertBoxHeight*1)/4);//
+//      LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        paramsLayoutSub.gravity=Gravity.CENTER_HORIZONTAL;
+        layoutSub.setLayoutParams(paramsLayoutSub);
+        layoutSub.setOrientation(LinearLayout.HORIZONTAL);
+        layoutSub.setBackgroundColor(Color.parseColor("#FFFFFF"));
+            
+        
+        
+        LinearLayout.LayoutParams paramsOkButtonCrash = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT);//(alertBoxWidth, alertBoxHeight);//(150, 70);
+        paramsOkButtonCrash.weight = 1.0f;
+        paramsOkButtonCrash.gravity=Gravity.CENTER;
+        paramsOkButtonCrash.bottomMargin = 10;
+        paramsOkButtonCrash.rightMargin = 20;
+        okButtonCrash.setLayoutParams(paramsOkButtonCrash);
+        okButtonCrash.setText("OK");
+        okButtonCrash.setBackgroundColor(Color.parseColor("#007FFF"));
+        
+        
+        LinearLayout.LayoutParams paramsCancelButtonCrash = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT);//(150, 70);
+        paramsCancelButtonCrash.weight = 1.0f;
+        paramsCancelButtonCrash.gravity=Gravity.CENTER;
+        paramsCancelButtonCrash.bottomMargin = 10;
+        paramsCancelButtonCrash.leftMargin = 20;
+        cancelButtonCrash.setLayoutParams(paramsCancelButtonCrash);
+        cancelButtonCrash.setText("Cancel");
+        cancelButtonCrash.setBackgroundColor(Color.parseColor("#007FFF"));
+    		    
+        layout.addView(textViewHeading);
+        layout.addView(textViewContent);
+        
+        layoutSub.setOrientation(LinearLayout.HORIZONTAL);
+        layoutSub.addView(okButtonCrash);
+        layoutSub.addView(cancelButtonCrash);
+        layout.addView(layoutSub);
+        
+        
+//        setContentView(layout);
+//        setContentView(R.layout.floatingactivity);
+        //** If user clicks OK, Send details to Server 
+       okButtonCrash.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
@@ -71,8 +167,8 @@ public class CrashReportActivity extends Activity {
 			}
 		});
         
-       /** If user clicks CANCEL, close the application */
-       ((Button)findViewById(R.id.cancelButtonCrash)).setOnClickListener(new OnClickListener() {			
+       //** If user clicks CANCEL, close the application 
+       cancelButtonCrash.setOnClickListener(new OnClickListener() {			
 			@Override
 			public void onClick(View v) {	
 				try {
@@ -86,9 +182,62 @@ public class CrashReportActivity extends Activity {
 				System.exit(0);
 			}
 		});
-        
+*/        
     }
     
+
+	private void openAlertDialog() {
+		// TODO Auto-generated method stub
+		AlertDialog.Builder builder = new AlertDialog.Builder(
+				CrashReportActivity.this);
+		builder.setCancelable(true);
+		builder.setTitle("ALERT");
+		builder.setMessage("Looks like something went wrong, Sorry for the inconvenience. We will analyze the issue.");
+		builder.setInverseBackgroundForced(true);
+		builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+
+				new loadClass(CrashReportActivity.this,(View)findViewById(
+						android.R.id.content).getRootView()).execute();
+				
+//				CrashReportActivity.this.finish();
+				dialog.dismiss();
+
+			}
+		});
+		builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				
+				try {
+					crashReportDB_Factory.deleteCrashReport(applicationInformation.getApplicationName(getBaseContext()), crashReportData.getCurrentDate());
+				} catch (NameNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				CrashReportActivity.this.finish();
+				dialog.dismiss();
+			}
+		});
+		AlertDialog alert = builder.create();
+		alert.show();
+		
+	 
+		Button b = alert.getButton(DialogInterface.BUTTON_NEGATIVE);
+		if (b != null) {
+//			b.setBackgroundResource(R.drawable.button_press);
+//			b.setTextColor(getResources().getColor(R.color.textcolor));
+		}
+		Button bq = alert.getButton(DialogInterface.BUTTON_POSITIVE);
+		if (bq != null) {
+//			bq.setBackgroundResource(R.drawable.button_press);
+//			bq.setTextColor(getResources().getColor(R.color.textcolor));
+		}
+	}
+
+
+
 	/**
 	 * Sub class to access the web service and to retrieve the response
 	 */
@@ -171,6 +320,6 @@ public class CrashReportActivity extends Activity {
 			}
 			
 		}
-
-	}    
+		
+	}	
 } 
